ac4c0da323e9f172e50f8a98a200ea4d75ee6f81
PauloRGNDev <168738853+PauloRGNDev@users.noreply.github.com>
2024-06-20 10:45:42 -0300

    add data of database to index

--- Life-Project/routes/index.js
+++ Life-Project/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require("../models");
+const SocialMedia = db.socialMedia;
+const Politic = db.politic;
+const Food = db.food;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {});
+router.get('/', async function(req, res, next) {
+  const foodsD = await Food.find().exec();
+  const politics = await Politic.find().exec();
+  const socialMedias = await SocialMedia.find().exec();
+  const foods = [{}];
+  const types = [{}];
+  for(let i = 0; i < 3; i++){
+    foods[i] = foodsD[i];
+    types[i].url = foodsD[i].typeUrl;
+  }
+  res.render('index', {foods: foods, types:types, politics:politics, socialMedias:socialMedias,});
 });
 
 module.exports = router;
