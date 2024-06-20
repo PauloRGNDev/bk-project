a76ec1ca7f9cb4600125c1a13b5fbd466f12aa83
PauloRGNDev <168738853+PauloRGNDev@users.noreply.github.com>
2024-06-20 11:50:47 -0300

    fix error with array declaration(declarating an empty obj in array)

--- Life-Project/routes/index.js
+++ Life-Project/routes/index.js
@@ -10,11 +10,11 @@ router.get('/', async function(req, res, next) {
   const foodsD = await Food.find().exec();
   const politics = await Politic.find().exec();
   const socialMedias = await SocialMedia.find().exec();
-  const foods = [{}];
-  const types = [{}];
+  const foods = [];
+  const types = [];
   for(let i = 0; i < 3; i++){
     foods[i] = foodsD[i];
-    types[i].url = foodsD[i].typeUrl;
+    types[i] = {url: foodsD[i].typeUrl};
   }
   res.render('index', {foods: foods, types:types, politics:politics, socialMedias:socialMedias,});
 });
