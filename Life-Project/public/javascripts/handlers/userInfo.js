4bd075de59372ed0d7582611d4db576317e23b93
PauloRGNDev <168738853+PauloRGNDev@users.noreply.github.com>
2024-06-25 14:30:20 -0300

    change how an page is redirected in login-info icon of index page

--- Life-Project/public/javascripts/handlers/userInfo.js
+++ Life-Project/public/javascripts/handlers/userInfo.js
@@ -1,7 +1,7 @@
 export async function signupClick(){
     try{
         console.log("clicked signup area");
-        window.location.replace('/auth/signup');
+        window.location.href = '/auth/signup';
     } catch(error){
         console.error("erro ao enviar mensagem: ", error);
     }
