5f613eb73367e893c16251b1a22c727576e8cf68
PauloRGNDev <168738853+PauloRGNDev@users.noreply.github.com>
2024-06-25 09:53:04 -0300

    adding debuggers, change path declaration in static js and add  type module to js import declaration in pug

--- Life-Project/public/javascripts/event_listeners/mouseClick.js
+++ Life-Project/public/javascripts/event_listeners/mouseClick.js
@@ -1,4 +1,6 @@
 const loginIcon = document.querySelector('html.initial-page .user-info');
 
-import {signupClick} from '../handlers/userInfo';
+import {signupClick} from '../handlers/userInfo.js';
 loginIcon.addEventListener('click', signupClick);
+
+console.log("Test");
--- Life-Project/public/javascripts/main.js
+++ Life-Project/public/javascripts/main.js
@@ -1,1 +1,2 @@
-import './event_listeners/mouseClick';
+import './event_listeners/mouseClick.js';
+console.log("Hello");
